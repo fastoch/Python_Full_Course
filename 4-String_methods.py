b09c002e80fee70fad83a808ca5b837264c6bb97
fastoch <fastoch@ik.me>
2023-01-15 18:59:23 +0100

    string methods 2

--- 4-String_methods.py
+++ 4-String_methods.py
@@ -1,17 +1,17 @@
 # Length method
-name = "fastOch"
+name = "fastoch"
 print(len(name))
 
-# find method - returns the index (position) of a character within a string
+# find() - returns the index (position) of a character within a string
 print(name.find("s")) # first position = index 0
 
-# capitalize method - first letter is uppercase
+# capitalize () - first letter is uppercase
 print(name.capitalize())
 
-# upper method - all letters are uppercase
+# upper () - all letters are uppercase
 print(name.upper())
 
-# lower method
+# lower ()
 print(name.lower())
 
 # isdigit()
@@ -19,5 +19,11 @@ print(name.isdigit())
 
 # isalpha() - does my string contain only alphabetical letters?
 print(name.isalpha())
+
+# count()
+print(name.count("o"))
+
+# replace()
+print(name.replace("o","i"))
 
 # 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Python Full Course - https://www.youtube.com/watch?v=XKHEtdqhLK8
-@23:00/12:00:00
+@24:00/12:00:00
 
 https://www.evernote.com/shard/s349/nl/229117830/012e0372-ba01-82da-2b7d-cc4026c7fe9b?title=Python%20Full%20Course
