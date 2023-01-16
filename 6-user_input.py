c865ec416fb9b501d9f62fcb101301d4ab83fbd4
fastoch <fastoch@ik.me>
2023-01-16 19:59:09 +0100

    user_input_over

---
+++ 6-user_input.py
@@ -0,0 +1,7 @@
+# user input
+name = input("What is your name? ")
+age = input("How old are you? ")
+weight = input("How much do you weigh? ")
+print("Hello "+name+"!")
+print("You're "+age+" years old.")
+print("You weigh "+weight+" kg.")
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Python Full Course - https://www.youtube.com/watch?v=XKHEtdqhLK8  
-@32:00/12:00:00
+@35:00/12:00:00
