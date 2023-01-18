badefbff3136bbf8d99f2296966765987a9df819
fastoch <fastoch@ik.me>
2023-01-18 17:34:21 +0100

    *

--- 9-if_statements.py
+++ 9-if_statements.py
@@ -1,1 +1,10 @@
 # if statement = a block of code if its conditions is true
+
+age = int(input("How old are you? "))
+
+if age >= 20:
+  print("You are a grown man now.")
+elif age > 12:
+  print("You are a teenager.")
+else:
+  print("You are still a kid.")
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Python Full Course - https://www.youtube.com/watch?v=XKHEtdqhLK8  
-@52:00/12:00:00
+@55:00/12:00:00
