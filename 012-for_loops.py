b495b0822593afc7df357f9e0aae5726501a5fbb
fastoch <fastoch@ik.me>
2023-01-19 18:27:52 +0100

    for_loops

--- 012-for_loops.py
+++ 012-for_loops.py
@@ -1,14 +1,18 @@
 # for loop =  a statement that will execute its block of code a limited amount of times
 # while loop = unlimited
 
-# will print numbers from 1 to 10
+# will print integers from 1 to 10
 for i in range(10):
     print(i+1)
 
-print("")
+print("") # line break
 
-# will print numbers from 50 to 59
+# will print integers from 50 to 59
 for i in range(50,60):
     print(i)
 
+print("")
 
+# will print all even integers from 30 to 50
+for i in range(30,51,2):
+    print(i)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Python Full Course - https://www.youtube.com/watch?v=XKHEtdqhLK8  
-@1:08:00/12:00:00
+@1:10:00/12:00:00
