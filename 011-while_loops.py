04c5d079cb682cac5004bf42abdb1ef068ddb65e
fastoch <fastoch@ik.me>
2023-01-19 17:59:56 +0100

    for_loops

--- 011-while_loops.py
+++ 011-while_loops.py
@@ -1,2 +1,7 @@
-# while loop = a statement that will execute its block of code as long as its condition remains true
+# while loop =  a statement that will execute its block of code as long as its condition remains true
 
+# the user will prompted until he enters a name
+name = ""
+while len(name) == 0:
+  name = input("Enter your name: ")
+print("Nice to meet you "+name+"!")
---
+++ 012-for_loops.py
@@ -0,0 +1,14 @@
+# for loop =  a statement that will execute its block of code a limited amount of times
+# while loop = unlimited
+
+# will print numbers from 1 to 10
+for i in range(10):
+    print(i+1)
+
+print("")
+
+# will print numbers from 50 to 59
+for i in range(50,60):
+    print(i)
+
+
