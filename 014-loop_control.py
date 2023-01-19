a4c6ce46b9fd3e5ce11bab7c7758b61eba0755fc
fastoch <fastoch@ik.me>
2023-01-19 21:30:41 +0100

    *

--- 014-loop_control.py
+++ 014-loop_control.py
@@ -1,1 +1,5 @@
-# Loop control statements = change a loop execution
+# Loop control statements = change a loop's execution from its normal sequence
+
+# break =       used to terminate the loop entirely
+# continue =    skips to the next iteration of the loop
+# pass =        does nothing, acts as a placeholder
