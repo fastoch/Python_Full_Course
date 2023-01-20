e46a1b6ae0ec97bb7d6a2f0690ad7089d4270d0c
fastoch <fastoch@ik.me>
2023-01-20 19:37:30 +0100

    *

--- 014-loop_control.py
+++ 014-loop_control.py
@@ -3,3 +3,16 @@
 # break =       used to terminate the loop entirely
 # continue =    skips to the next iteration of the loop
 # pass =        does nothing, acts as a placeholder
+
+while True:
+    name = input("What's your name? ")
+    if name != "":
+        break
+
+phone_number = "123-456-7890"
+# let's display this phone number without the dashes
+for i in phone_number:
+    if i == "-":
+        continue
+    print(i, end="")
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Python Full Course - https://www.youtube.com/watch?v=XKHEtdqhLK8  
-@1:17:00/12:00:00
+@1:20:00/12:00:00
