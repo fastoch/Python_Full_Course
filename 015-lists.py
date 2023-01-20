0181e30ec4af828b5c3fea119ca149ed4450cbbc
fastoch <fastoch@ik.me>
2023-01-20 21:07:13 +0100

    *

--- 015-lists.py
+++ 015-lists.py
@@ -1,3 +1,8 @@
 # list = used to store multiple items in a single variable
 
-food = ["pizza","hamburger","",""]
+food = ["pizza","hamburger","prosciutto","spaghetti"]
+print(food)
+print(food[1])
+print(food[0])
+print(len(food))
+print(type(food))
