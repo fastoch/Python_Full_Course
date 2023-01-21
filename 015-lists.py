ecefdf095d642a21a545c4faed78c1acdfa81c25
fastoch <fastoch@ik.me>
2023-01-21 10:44:35 +0100

    *

--- 015-lists.py
+++ 015-lists.py
@@ -1,8 +1,32 @@
 # list = used to store multiple items in a single variable
 
 food = ["pizza","hamburger","prosciutto","spaghetti"]
+
+print("Here is my food list: "+str(food))
+print("food is a variable which type is: "+str(type(food)))
+print("This food list contains "+str(len(food))+" items.")
+print("The first item in the list is: "+food[0])
+print("The second item in this list is: "+food[1])
+
+food[0] = "sushi"
+print("I replaced the first item by: "+food[0])
+
+print("Here are the items of my new list:")
+for x in food:
+    print(x)
+
+# add an item to the list
+food.append("paëlla")
 print(food)
-print(food[1])
-print(food[0])
-print(len(food))
-print(type(food))
+
+food.remove("hamburger")
+print(food)
+
+# remove the last element
+food.pop()
+print(food)
+
+# insert an item at a given index
+food.insert(1,"shrimps")
+print(food)
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Python Full Course - https://www.youtube.com/watch?v=XKHEtdqhLK8  
-@1:24:00/12:00:00
+@1:26:00/12:00:00
