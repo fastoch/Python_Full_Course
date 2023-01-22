7aa0da6a76b12f5e84789c727ddb9f2ea0f934b3
fastoch <fastoch@ik.me>
2023-01-22 12:19:51 +0100

    functions 2 parameters

---
+++ 021-functions.py
@@ -0,0 +1,12 @@
+# function = a block of code which is executed only when it is called (invoked).
+
+def hello(name1,name2):
+    print("Hello "+name1+" and "+name2+"!")
+    print("Have a nice day!\n")
+
+nickname1 = "fast&curious"
+nickname2 = "balane"
+
+# calling (invoking) the hello() function
+hello("fastoch","Sandro")
+hello(nickname1,nickname2)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Python Full Course - https://www.youtube.com/watch?v=XKHEtdqhLK8  
-1:53:00/12:00:00
+2:00:00/12:00:00
