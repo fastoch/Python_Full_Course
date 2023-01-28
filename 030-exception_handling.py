1c06264b40cde45d9970a2a4dcff2f954eb8c167
fastoch <fastoch@ik.me>
2023-01-28 14:17:34 +0100

    file detection

--- 030-exception_handling.py
+++ 030-exception_handling.py
@@ -6,11 +6,26 @@ try:
     numerator = int(input("Enter a number to divide: "))
     denominator = int(input("Enter a number to divide by: "))
     result = numerator/denominator
-    print(result)
 # if denominator == 0, this will cause an exception called "ZeroDivisionError"
 # ZeroDivisionError is a built-in exception
-# we can replace this built-in exception handling with our own 
-except ZeroDivisionError:
+# we can customize the handling of this built-in exception 
+except ZeroDivisionError as e:
+    print(e)
     print("You can't divide by zero, you moron!")
-except Exception:
+except ValueError as e:
+    print(e)
+    print("Enter only numbers, please.")
+# It's considered good practice to first catch specific exceptions, and let the user know exactly what went wrong.
+
+except Exception as e: # just in case there are exceptions we didn't anticipate
+    print(e)
     print("Something went wrong :(")
+
+# in case we don't catch any exception, we execute what's in the else block
+else: 
+    print("Congratulations! We didn't catch any exception.")
+    print("The result is: " + str(result))
+
+# whether or not we catch an exception, we will always execute what's in the finally block
+finally:
+    print("Whether we catch an exception or not, this will always execute.")
