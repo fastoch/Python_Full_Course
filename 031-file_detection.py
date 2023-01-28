25d2e39aeaa83aeaee1edcc8a16e5706dbf8dd59
fastoch <fastoch@ik.me>
2023-01-28 16:38:17 +0100

    file detection

--- 031-file_detection.py
+++ 031-file_detection.py
@@ -1,2 +1,14 @@
+# For file detection, we need the os module 
+# This module is already included with the standard python library. All we need to do is import it
 import os
 
+# we want to check if a file exists somewhere on our computer
+path = "/home/fastoch/Documents/test.txt"
+if os.path.exists(path):
+    print("That location exists")
+    if os.path.isfile(path):
+        print("That is a file")
+    elif os.path.isdir(path):
+        print("That is a directory")
+else: 
+    print("That location does not exist")
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Python Full Course - https://www.youtube.com/watch?v=XKHEtdqhLK8  
-2:44:00/12:00:00
+2:47:00/12:00:00
