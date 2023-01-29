7b0eaab84b0b68c77f85cbdaca24c2e8adede740
fastoch <fastoch@ik.me>
2023-01-29 12:02:44 +0100

    copying files

--- 034-copying_files.py
+++ 034-copying_files.py
@@ -1,1 +1,7 @@
-# copyfile() = 
+# copyfile() = copies contents of a file
+# copy() = copyfile() + permission mode + destination can be a directory
+# copy2() = copy() + copy metadata (file's creation and modification times)
+
+import shutil # The shutil module helps in automating the process of copying and removal of files and directories
+
+shutil.copyfile('/home/fastoch/Documents/test.txt','/home/fastoch/Downloads/copy.txt') # 2 args: (source,destination)
