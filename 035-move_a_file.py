5e73060dce70d8b3964f28a3c16b58483dd7975b
fastoch <fastoch@ik.me>
2023-01-29 13:08:07 +0100

    delete a file

--- 035-move_a_file.py
+++ 035-move_a_file.py
@@ -1,2 +1,15 @@
 import os
 
+source = "/home/fastoch/Documents/test2.txt"
+destination = "/home/fastoch/Desktop/moved_test2.txt"
+
+try:
+    if os.path.exists(destination):
+        print("There is already a file there")
+    else:
+        os.replace(source,destination)
+        print(source+" was moved")
+except FileNotFoundError:
+    print("'"+source+"' was not found")
+
+# This program can also be used to move folders
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Python Full Course - https://www.youtube.com/watch?v=XKHEtdqhLK8  
-2:58:00/12:00:00
+3:01:00/12:00:00
